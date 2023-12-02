eb699608cd71173264a1025182a01c37fe3ebf2a
edoster <edoster@uoregon.edu>
2023-12-01 19:47:43 -0800

    New logic for requirements and models.py

--- api/database/models.py
+++ api/database/models.py
@@ -4,7 +4,7 @@ from mongoengine import *
 class Checkpoint(EmbeddedDocument):
     """
     A MongoEngine EmbeddedDocument containing:
-        distance: MongoEngine float field, required, (checkpoint distance in kilometers),
+        km/miles: MongoEngine float field, required, (checkpoint distance in kilometers),
 		location: MongoEngine string field, optional, (checkpoint location name),
 		open_time: MongoEngine datetime field, required, (checkpoint opening time),
 		close_time: MongoEngine datetime field, required, (checkpoint closing time).
@@ -12,17 +12,17 @@ class Checkpoint(EmbeddedDocument):
     km = FloatField(required=True)
     miles = FloatField(required=True)
     location = StringField()
-    open_time = DateTimeField(required=True,tzinfo=True)
-    close_time = DateTimeField(required=True,tzinfo=True)
+    open_time = DateTimeField(required=True)
+    close_time = DateTimeField(required=True)
 
 
 class Brevet(Document):
     """
     A MongoEngine document containing:
-		length: MongoEngine float field, required
-		start_time: MongoEngine datetime field, required
-		checkpoints: MongoEngine list field of Checkpoints, required
+		distance: MongoEngine float field, required
+		begin_date: MongoEngine datetime field, required
+		controls: MongoEngine list field of Checkpoints, required
     """
-    distance = FloatField(required=True) # might need fixing
-    begin_date = DateTimeField(required=True,tzinfo=True)
+    distance = FloatField(required=True) 
+    begin_date = DateTimeField(required=True)
     controls = ListField(EmbeddedDocumentField(Checkpoint), required=True)
